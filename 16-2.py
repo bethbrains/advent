25c498b5fdb51574dc587c4d0b2fe6d1852f8166
Beth Breisnes <bbreisnes@salesforce.com>
2019-12-17 13:12:44 -0800

    16-2 inefficient

--- 16-2.py
+++ 16-2.py
@@ -11,18 +11,12 @@ with open("16input.txt") as fp:
     offset = input_list[:7]
     pattern = [0, 1, 0, -1]
     next_input = []
-    patterns_by_step = {}
-    for step in range(1, len(input_list) + 1):
-        print(step)
-        # patterns_by_step[step] = [pattern[(x+1) // step % 4] for x in range(len(input_list))]
-    print(patterns_by_step)
     for j in range(phase, max_phase + 1):
         print('new phase: ' + str(j))
         for step in range(1, len(input_list) + 1):
-            # patt_for_step = patterns_by_step[step]
             sumprod = sum(x * pattern[(x+1) // step % 4] for x in input_list)
             ones_digit = abs(sumprod) // 1 % 10
             next_input.append(ones_digit)
         input_list = next_input
         next_input = []
-    print("".join([str(x) for x in input_list[offset:8]]))
+    print("".join([str(x) for x in input_list[:8]]))
