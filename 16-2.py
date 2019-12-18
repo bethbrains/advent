ccbc3f139a5f5ea5c1d3af29b1e828ebe56ba6a8
Beth Breisnes <bbreisnes@salesforce.com>
2019-12-18 14:33:50 -0800

    indexing technique, still wip

--- 16-2.py
+++ 16-2.py
@@ -1,22 +1,44 @@
-def sum_product(input, pattern):
-    lists = input, pattern
-    return sum([x * y for x, y in zip(*lists)])
-
-
 with open("16input.txt") as fp:
-    input_list = [int(x) for x in list(fp.read())] * 10000
+    input = [int(x) for x in list(fp.read())] * 10000
     phase = 1
     max_phase = 4
-    length = len(input_list)
-    offset = input_list[:7]
+    length = len(input)
+    offset = int("".join([str(x) for x in input[:7]]))
+    print(offset)
     pattern = [0, 1, 0, -1]
     next_input = []
     for j in range(phase, max_phase + 1):
         print('new phase: ' + str(j))
-        for step in range(1, len(input_list) + 1):
-            sumprod = sum(x * pattern[(x+1) // step % 4] for x in input_list)
+        for row in range(1, len(input) + 1):
+            print('ROW' + str(row))
+            # print(input)
+
+            sumprod = 0
+
+            start_add_slice = row-1
+
+            if row > length // 2:
+                sumprod = sum(input[start_add_slice:])
+            else:
+                start_sub_slice = row * 3 - 1
+
+                while start_add_slice < length:
+                    # print('ADDING')
+                    # print(sum(input[start_slice:start_slice + row]))
+                    sumprod += sum(input[start_add_slice:start_add_slice + row])
+                    start_add_slice += (row * 4)
+
+                    if row < length // 4:
+                        # print('SUBTRACTING')
+                        # print(sum(input[start_slice:start_slice + row]))
+                        sumprod -= sum(input[start_sub_slice:start_sub_slice + row])
+                        start_sub_slice += row * 4
+
+            # print(sumprod)
             ones_digit = abs(sumprod) // 1 % 10
+            # print(ones_digit)
             next_input.append(ones_digit)
-        input_list = next_input
+        # print(next_input)
+        input = next_input
         next_input = []
-    print("".join([str(x) for x in input_list[:8]]))
+    print("".join([str(x) for x in input[offset:8]]))
